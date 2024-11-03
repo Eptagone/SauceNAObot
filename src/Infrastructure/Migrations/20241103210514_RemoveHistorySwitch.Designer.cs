9114d71cd96cb18b0838fe08296a0270284e0935
Quetzal Rivera <quetzaldeveloper@outlook.com>
2024-11-03 15:05:41 -0600

    fix: Remove history switch field

--- src/Infrastructure/Migrations/20241103210514_RemoveHistorySwitch.Designer.cs
+++ src/Infrastructure/Migrations/20241103210514_RemoveHistorySwitch.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SauceNAO.Infrastructure.Data;
@@ -11,9 +12,11 @@ using SauceNAO.Infrastructure.Data;
 namespace SauceNAO.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241103210514_RemoveHistorySwitch")]
+    partial class RemoveHistorySwitch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Migrations/20241103210514_RemoveHistorySwitch.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SauceNAO.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveHistorySwitch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SaveRecentSauces",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SaveRecentSauces",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
