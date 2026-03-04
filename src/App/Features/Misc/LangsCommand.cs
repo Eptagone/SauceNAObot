fc53917bc240acb227831e7de99e430463752f90
Quetzal Rivera <quetzaldeveloper@outlook.com>
2026-03-03 21:52:15 -0600

    feat: re-add langs command

---
+++ src/App/Features/Misc/LangsCommand.cs
@@ -0,0 +1,60 @@
+using Microsoft.Extensions.Caching.Distributed;
+using SauceNAO.App.Resources;
+using SauceNAO.Core.Data;
+using SauceNAO.Core.Services;
+using Telegram.BotAPI;
+using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.AvailableTypes;
+using Telegram.BotAPI.Extensions.Commands;
+
+namespace SauceNAO.App.Features.Misc;
+
+[BotCommand("langs", "List the supported languages")]
+[BotCommandVisibility(BotCommandVisibility.Hidden)]
+class LangsCommand(
+    IContextProvider contextProvider,
+    ITelegramBotClient client,
+    IUserRepository userRepository,
+    IBetterStringLocalizer<LangsCommand> localizer,
+    IDistributedCache cache
+) : ICommandHandler
+{
+    public async Task InvokeAsync(
+        Message message,
+        string[] args,
+        CancellationToken cancellationToken
+    )
+    {
+        await client.SendChatActionAsync(
+            message.Chat.Id,
+            ChatActions.Typing,
+            cancellationToken: cancellationToken
+        );
+        await contextProvider.LoadAsync(message, cancellationToken);
+        var languages = await cache.GetStringAsync("languages", cancellationToken);
+        if (languages is null)
+        {
+            var languageCodes = await userRepository.GetLanguageCodesAsync(cancellationToken);
+            languages = string.Join(
+                "\n",
+                languageCodes
+                    .OrderByDescending(lc => lc.Value)
+                    .Select(lc => $"<b>{lc.Key}:</b> [{lc.Value}]")
+            );
+            await cache.SetStringAsync("languages", languages, cancellationToken);
+        }
+
+        var text = localizer["Languages", languages];
+        await client.SendMessageAsync(
+            message.Chat.Id,
+            text,
+            replyParameters: new()
+            {
+                AllowSendingWithoutReply = true,
+                MessageId = message.MessageId,
+            },
+            parseMode: FormatStyles.HTML,
+            cancellationToken: cancellationToken
+        );
+    }
+}
